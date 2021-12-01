b22542a239dab9858d7f129e4b52af5fce249cd3
sachin goel <goelsachin955@gmail.com>
2021-12-01 14:26:49 +0530

    captche function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'captche';
+  catpcheCountOne: number = Math.floor(Math.random() * 10);
+  catpcheCountTwo: number = Math.floor(Math.random() * 10);
+  result: any;
+  userValue: any;
+
+  getRandomCount(myForm: any) {
+    this.catpcheCountOne = Math.floor(Math.random() * 10);
+    this.catpcheCountTwo = Math.floor(Math.random() * 10);
+    this.userValue = ""
+    
+  }
+
+  submitHandler(myForm: any) {
+    this.result = this.catpcheCountOne + this.catpcheCountTwo;
+    console.log(this.result, 'correct value');
+    
+    if (this.result === Number(myForm.value.captche_value)) {
+      alert('succesful login');
+      console.log(myForm.value.captche_value, 'right value');
+    } else {
+      alert('Wrong Capcha value');
+      console.log(myForm.value.captche_value, 'wrong value');
+    }
+  }
 }
